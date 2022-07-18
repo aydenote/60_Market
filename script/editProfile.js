7ab9bd124053390b42f3399fa531f1fde69c66c2
aydenote <dslgpgh@gmail.com>
2022-07-18 11:22:58 +0900

    ✨ Feat : input 사용자 ID 추가, 글자수 체크 후 버튼 활성화 기능

--- script/editProfile.js
+++ script/editProfile.js
@@ -1,11 +1,16 @@
-// input Name, input introduce 모두 2글자 이상 입력 시 헤더바에 저장 버튼 활성화
+// input Name, input introduce 2글자 이상, input id 5글자 이상 입력 시 헤더바에 저장 버튼 활성화
 const profileSaveButton = document.querySelector(".headerBarBtn.buttonClick");
+const inputName = document.querySelector(".profileModificationForm #name");
+const inputId = document.querySelector(".profileModificationForm #id");
+const inputIntroduce = document.querySelector(".profileModificationForm #introduce");
 
 profileSaveButton.disabled = true;
 function profileChangeInput() {
   const inputNameLength = document.querySelector(".profileModificationForm #name").value.length;
   const inputIntroduceLength = document.querySelector(".profileModificationForm #introduce").value.length;
-  if (inputNameLength >= 2 && inputIntroduceLength >= 2) {
+  const inputIdLength = document.querySelector(".profileModificationForm #id").value.length;
+
+  if (inputNameLength >= 2 && inputIntroduceLength >= 2 && inputIdLength >= 5) {
     profileSaveButton.style.opacity = "1";
     profileSaveButton.disabled = false;
   } else {
