deb722534d7bcbb6236b2bf86015e7444a7f5363
ujin <dklvmf0106@gmail.com>
2022-07-21 14:18:07 +0900

    ✨ Fea: 프로필 페이지 게시물 타입 버튼 기능 추가

--- script/home.js
+++ script/home.js
@@ -1,18 +1,25 @@
 //로그인 구현시 삭제
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYjNmOGUxNTM2MWFhZWE1NjlhYWUzOCIsImV4cCI6MTY2MTE0NTgyNywiaWF0IjoxNjU1OTYxODI3fQ.F7JLkiPLzbW3GcLUK_b_-h4_7zkZdLQgTZB-OddOhLY";
-// const accountName = localStorage.getItem("accountname");
-const accountName = "ujin16";
+
 let state = "list";
 const curUrl = location.href;
+
+const albumBtn = document.querySelector(".postingType.album");
+const listBtn = document.querySelector(".postingType.list");
+const albumContent = document.querySelector(".postContent");
+const listContent = document.querySelector(".post");
+
+listBtn.addEventListener("click", changeListType);
+albumBtn.addEventListener("click", changeAlbumType);
 
 // 프로필페이지 게시물 리스트형으로 변경, 버튼 활성화
 function changeListType() {
   state = "list";
   albumContent.classList.add("hidden");
   listContent.classList.remove("hidden");
-  albumBtn.classList.add(".unselected");
-  listBtn.classList.remove(".unselected");
+  albumBtn.classList.add("unselected");
+  listBtn.classList.remove("unselected");
 }
 
 // 프로필페이지 게시물 앨범형으로 변경, 버튼 활성화
@@ -20,6 +27,6 @@ function changeAlbumType() {
   state = "album";
   albumContent.classList.remove("hidden");
   listContent.classList.add("hidden");
-  albumBtn.classList.remove(".unselected");
-  listBtn.classList.add(".unselected");
+  albumBtn.classList.remove("unselected");
+  listBtn.classList.add("unselected");
 }
