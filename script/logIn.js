10cb9a8bb55d3d3fd90599d7e2c644f5c352a553
moeyg <moooeyg@gmail.com>
2022-07-24 22:45:24 +0900

    ✨ Feat : 폼 입력 이벤트, 비밀번호 길이 체크, 버튼 활성화 추가

--- script/logIn.js
+++ script/logIn.js
@@ -4,3 +4,23 @@ const password = document.querySelector('#password');
 const errorMessage = document.querySelector('.registerFormAlert');
 const logInForm = document.querySelector('.logInForm');
 const logInFormBtn = document.querySelector('.logInBtn');
+
+// 버튼 활성화
+const isActiveBtn = () => {
+  if (email.value !== '' && password.value !== '') {
+    logInFormBtn.style.opacity = '1';
+    logInFormBtn.disabled = false;
+  }
+};
+
+// 비밀번호 길이 체크
+const checkPassword = () => {
+  password.addEventListener('input', () => {
+    if (password.value.length > 6) {
+      isActiveBtn();
+    }
+  });
+};
+
+// 폼 입력
+logInForm.addEventListener('input', checkPassword);
