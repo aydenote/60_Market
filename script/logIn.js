beb9d7250b94af07b494a596c8685616ee8449b7
moeyg <moooeyg@gmail.com>
2022-07-24 22:44:11 +0900

    ➕ Add : logIn.js

---
+++ script/logIn.js
@@ -0,0 +1,6 @@
+const url = 'https://mandarin.api.weniv.co.kr';
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const errorMessage = document.querySelector('.registerFormAlert');
+const logInForm = document.querySelector('.logInForm');
+const logInFormBtn = document.querySelector('.logInBtn');
