28bc5d968ea561fcac8468c397b815a90805fd69
moeyg <moooeyg@gmail.com>
2022-07-24 23:36:50 +0900

    ⚙ Refactor : 폼 입력, 비밀번호 길이 체크, 버튼 활성화 리팩토링

--- script/signUp.js
+++ script/signUp.js
@@ -6,66 +6,67 @@ const errorPassword = document.querySelector('.errorPassword');
 const registerForm = document.querySelector('.registerForm');
 const registerFormBtn = document.querySelector('.registerFormBtn');
 
-// 회원가입 버튼 활성화
-function isActiveBtn() {
-  if (email.value !== '' && password.value !== '') {
+// 버튼 활성화
+const isActiveBtn = () => {
+  if (email.value !== '' && password.value.length > 6) {
     registerFormBtn.style.opacity = '1';
     registerFormBtn.disabled = false;
-  } else {
+  }
+  if (email.value === '' || password.value.length <= 6) {
+    registerFormBtn.style.opacity = '0.3';
     registerFormBtn.disabled = true;
   }
-}
-
-// 사용 가능한 이메일 체크
-function checkEmail(resMessage) {
-  if (resMessage !== '사용 가능한 이메일 입니다.') {
-    errorEmail.innerText = `*${resMessage}`;
-    errorEmail.classList.remove('ir');
-    email.oninput = () => {
-      errorEmail.classList.add('ir');
-    };
-  }
-}
+};
 
 // 비밀번호 길이 체크
-password.addEventListener('input', () => {
+const checkPassword = password.addEventListener('input', () => {
   if (password.value.length < 6) {
     errorPassword.classList.remove('ir');
-    registerFormBtn.disabled = true;
-  } else {
+  }
+  if (password.value.length >= 6) {
     errorPassword.classList.add('ir');
-    registerFormBtn.disabled = false;
     isActiveBtn();
   }
 });
 
 // 폼 입력
-registerForm.addEventListener('input', isActiveBtn);
+registerForm.addEventListener('input', checkPassword);
 
-// 유효한 이메일 검사
-async function isValidEmail() {
-  try {
-    const res = await axios.post(`${url}/user/emailvalid`, {
-      user: {
-        email: email.value,
-      },
-    });
-    const resMessage = res.data.message;
-    checkEmail(resMessage);
-    return resMessage;
-  } catch (err) {
-    console.log(err);
-    checkEmail(err.response.data.message);
-  }
-}
+// // 사용 가능한 이메일 체크
+// const checkEmail = (resMessage) => {
+//   if (resMessage !== '사용 가능한 이메일 입니다.') {
+//     errorEmail.innerText = `*${resMessage}`;
+//     errorEmail.classList.remove('ir');
+//     email.oninput = () => {
+//       errorEmail.classList.add('ir');
+//     };
+//   }
+// }
 
-// 프로필 설정 페이지로 이동
-registerFormBtn.addEventListener('click', locationRegisterProfile);
+// // 유효한 이메일 검사
+// async function isValidEmail() {
+//   try {
+//     const res = await axios.post(`${url}/user/emailvalid`, {
+//       user: {
+//         email: email.value,
+//       },
+//     });
+//     const resMessage = res.data.message;
+//     checkEmail(resMessage);
+//     return resMessage;
+//   } catch (err) {
+//     console.log(err);
+//     checkEmail(err.response.data.message);
+//   }
+// }
 
-async function locationRegisterProfile(event) {
-  event.preventDefault();
-  const isValidResult = await isValidEmail();
-  if (isValidResult === '사용 가능한 이메일 입니다.') {
-    location.href = './registerProfile.html';
-  }
-}
+// // 프로필 설정 페이지로 이동
+// registerFormBtn.addEventListener('click', locationRegisterProfile);
+
+// async function locationRegisterProfile(event) {
+//   event.preventDefault();
+//   const isValidResult = await isValidEmail();
+//   if (isValidResult === '사용 가능한 이메일 입니다.') {
+//     location.href = './registerProfile.html';
+//   }
+// }
