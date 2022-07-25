9a7d563ebbb521480a2d237a111f1663b3c42ebe
moeyg <moooeyg@gmail.com>
2022-07-25 20:50:52 +0900

    ➕ Add : registerProdfile.js

---
+++ script/registerProfile.js
@@ -0,0 +1,7 @@
+const url = 'https://mandarin.api.weniv.co.kr';
+const username = document.querySelector('#name');
+const id = document.querySelector('#id');
+const introduce = document.querySelector('.introduce');
+const profileImg = document.querySelector('#profileImg');
+const profileFormBtn = document.querySelector('.profileFormBtn');
+const errorId = document.querySelector('.errorId');
