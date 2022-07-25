0a804420afba0313b8d11ba7bb6b8d9e67a922b0
Hongryeoll <hong462804@gmail.com>
2022-07-26 00:47:11 +0900

    ✨ Feat : 모달창구현 기능 추가

--- script/post.js
+++ script/post.js
@@ -13,3 +13,18 @@ function postInput() {
     postButton.disabled = true;
   }
 }
+
+// 모달창 구현
+const API_ROOT = 'https://mandarin.api.weniv.co.kr';
+const POST_ID = new URLSearchParams(location.search).get('postid'); 
+
+async function renderPost() {
+  const token = localStorage.getItem('Token');
+  try {
+    const res = await fetch(`${API_ROOT}/post/${POST_ID}`, {
+      method: 'GET',
+      headers: {
+        'Authorization' : `Bearer ${token}`,
+	      'Content-type' : 'application/json',
+      }
+    });
