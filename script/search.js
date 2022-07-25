ee5be62d8270494e3745f2557a587df3241de53b
ujin <dklvmf0106@gmail.com>
2022-07-25 15:10:08 +0900

    ⚙️ Refactor : 검색 이벤트 핸들러 비동기 처리

--- script/search.js
+++ script/search.js
@@ -1,53 +1,53 @@
+const url = "https://mandarin.api.weniv.co.kr";
+const token = localStorage.getItem("Token");
 const searchInput = document.querySelector(".headerBarSearch");
 const userListContent = document.querySelector(".userList");
 
-//로그인 구현시 삭제
-const token = localStorage.getItem("Token");
+async function searchUser(e) {
+  try {
+    if (e.target.value == "") {
+      userListContent.innerHTML = "";
+    } else {
+      const res = await fetch(
+        `${url}/user/searchuser/?keyword=${e.target.value}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const userData = await res.json();
 
-searchInput.addEventListener("keyup", (e) => {
-  if (e.target.value == "") {
-    userListContent.innerHTML = "";
-  } else {
-    searchUserList(e.target.value);
+      userListContent.innerHTML = "";
+      userData.forEach((element) => {
+        userListContent.innerHTML += `
+          <li class="userItem">
+              <a href="profile.html\?accountname=${element.accountname}"
+              class="userBox">
+                  <img
+                      src="${element.image}"
+                      alt="${element.username}님의 프로필 이미지"
+                      class="userProfileImage"
+                  />
+                  <div class="userInfo">
+                      <strong class="userNickname">${element.username}</strong>
+                      <div class="userText">
+                          <p class="userMsgContent userStatusMsg">
+                              @${element.accountname}
+                          </p>
+                      </div>
+                  </div>
+                  <button class="followBtn">팔로우</button>
+              </a>
+          </li>
+          `;
+      });
+    }
+  } catch (err) {
+    console.log(err);
   }
-});
-
-async function searchUserList(inputValue) {
-  userListContent.innerHTML = "";
-
-  const url = `https://mandarin.api.weniv.co.kr/user/searchuser/?keyword=${inputValue}`;
-
-  const res = await fetch(url, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-type": "application/json",
-    },
-  });
-
-  const data = await res.json();
-
-  data.forEach((element) => {
-    userListContent.innerHTML += `
-    <li class="userItem" id="${element._id}">
-        <a href="profile.html\?accountname=${element.accountname}"
-        class="userBox">
-        <img
-            src="${element.image}"
-            alt="${element.username}님의 프로필 이미지"
-            class="userProfileImage"
-        />
-        <div class="userInfo">
-            <strong class="userNickname">${element.username}</strong>
-            <div class="userText">
-            <p class="userMsgContent userStatusMsg">
-                @${element.accountname}
-            </p>
-            </div>
-        </div>
-        <button class="followBtn">팔로우</button>
-        </a>
-    </li>
-    `;
-  });
 }
+
+searchInput.addEventListener("input", searchUser);
