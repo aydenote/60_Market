d5c1a2fde1acdd5dd4e7900c127cae582f8efb58
ujin <dklvmf0106@gmail.com>
2022-07-26 08:17:37 +0900

    ✨ Feat :  토큰 없을 경우 index.html로 이동

--- script/common.js
+++ script/common.js
@@ -1,3 +1,8 @@
 // 공통으로 사용되는 코드
 const token = localStorage.getItem("Token");
 const accountname = localStorage.getItem("accountname");
+
+// 토크 없을 경우
+if (!token) {
+  window.location.href = "index.html";
+}
