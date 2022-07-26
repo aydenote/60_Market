392f3b91ee01c31eca4637ee4f0ffe9805b694e4
aydenote <dslgpgh@gmail.com>
2022-07-26 19:39:36 +0900

    ⚙️ Refactor : 뒤로가기 버튼 기능 개선

--- script/common.js
+++ script/common.js
@@ -1,18 +1,17 @@
 // 공통으로 사용되는 코드
-const token = localStorage.getItem("Token");
 const accountname = localStorage.getItem("accountname");
 
 // 토크 없을 경우
-if (!token) {
-  window.location.href = "index.html";
-}
+// if (!token) {
+//   window.location.href = "index.html";
+// }
 
 // 뒤로가기
 const backBtn1 = document.querySelector(".headerBarBack");
 const backBtn2 = document.querySelector(".backBtn");
 
 function backHistory() {
-  window.history.back();
+  window.location = document.referrer;
 }
 backBtn1.addEventListener("click", backHistory);
 backBtn2.addEventListener("click", backHistory);
