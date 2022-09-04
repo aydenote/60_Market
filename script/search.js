f06569c8c35820d0d1f5e1bc3ecd290eb2d4d770
aydenote <dslgpgh@gmail.com>
2022-09-04 19:44:09 +0900

    검색 시 유저 정보 구현 및 유저 클릭 시 url 변경 구현

--- script/search.js
+++ script/search.js
@@ -1,3 +1,9 @@
+import App from "./app.js";
+
+const config = {
+  rootEl: "#root",
+};
+
 export default async function searchUser(e, url, token, userListContent) {
   try {
     if (e.target.value == "") {
@@ -15,28 +21,46 @@ export default async function searchUser(e, url, token, userListContent) {
       );
       const userData = await res.json();
 
+      // 검색된 유저 정보 구현
       userListContent.innerHTML = "";
       userData.forEach((element) => {
-        userListContent.innerHTML += `
-          <li class="userItem">
-              <a href="profile.html\?accountname=${element.accountname}"
-              class="userBox">
-                  <img
-                      src="${element.image}"
-                      alt="${element.username}님의 프로필 이미지"
-                      class="userProfileImage"
-                  />
-                  <div class="userInfo">
-                      <strong class="userNickname">${element.username}</strong>
-                      <div class="userText">
-                          <p class="userMsgContent userStatusMsg">
-                              @${element.accountname}
-                          </p>
-                      </div>
-                  </div>
-              </a>
-          </li>
-          `;
+        const userItemEl = document.createElement("li");
+        const userAnchorEl = document.createElement("a");
+        const userImgEl = document.createElement("img");
+        const userInfoEl = document.createElement("div");
+        const nickNameEl = document.createElement("strong");
+        const userText = document.createElement("div");
+        const accountNameEl = document.createElement("p");
+
+        userItemEl.classList.add("userItem");
+        userAnchorEl.classList.add("userBox");
+        userAnchorEl.addEventListener("click", () => {
+          window.history.pushState(
+            {},
+            "",
+            `profile\?accountname=${element.accountname}`
+          ); // 주소 업데이트
+          new App(config).setup();
+        });
+        userImgEl.classList.add("userProfileImage");
+        userImgEl.setAttribute("src", `${element.image}`);
+        userImgEl.setAttribute("alt", `${element.username}님의 프로필 이미지`);
+        userInfoEl.classList.add("userInfo");
+        nickNameEl.classList.add("userNickname");
+        nickNameEl.innerText = `${element.username}`;
+        userText.classList.add("userText");
+        accountNameEl.classList.add("userMsgContent");
+        accountNameEl.classList.add("userStatusMsg");
+        accountNameEl.innerText = `@${element.accountname}`;
+
+        userText.appendChild(accountNameEl);
+        userInfoEl.appendChild(nickNameEl);
+        userInfoEl.appendChild(userText);
+        userAnchorEl.appendChild(userImgEl);
+        userAnchorEl.appendChild(userInfoEl);
+        userItemEl.appendChild(userAnchorEl);
+
+        userListContent.appendChild(userItemEl);
       });
     }
   } catch (err) {
