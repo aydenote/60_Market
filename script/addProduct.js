e4689facc2fdc4a0c844484a0e8b82b5d9b923c1
aydenote <dslgpgh@gmail.com>
2022-09-19 21:55:05 +0900

    상품 등록 마크업 완료

--- script/addProduct.js
+++ script/addProduct.js
@@ -1,108 +1,100 @@
-const backHistory = document.querySelector(".headerBarBack.buttonClick");
-const productForm = document.querySelector('.addProductForm');
-const productImgForm = document.querySelector('#addProductImg');
-const productNameForm = document.querySelector('#addProductName');
-const productPriceForm = document.querySelector('#addProductPrice');
-const productContentForm = document.querySelector('#addProductContent');
-const productSaveButton = document.querySelector('#addProductSaveBtn');
-const timeSelectlist = document.querySelector('.addProductTimeList');
-const timeSelelectBtn = document.querySelector(".addProductSelectBtn");
-const timeSelectList = document.querySelector(".addProductTimeList");
-const url = "https://mandarin.api.weniv.co.kr";
-let imageUrl;
+// const backHistory = document.querySelector(".headerBarBack.buttonClick");
+// const productForm = document.querySelector('.addProductForm');
+// const productImgForm = document.querySelector('#addProductImg');
+// const productNameForm = document.querySelector('#addProductName');
+// const productPriceForm = document.querySelector('#addProductPrice');
+// const productContentForm = document.querySelector('#addProductContent');
+// const productSaveButton = document.querySelector('#addProductSaveBtn');
+// const timeSelectlist = document.querySelector('.addProductTimeList');
+// const timeSelelectBtn = document.querySelector(".addProductSelectBtn");
+// const timeSelectList = document.querySelector(".addProductTimeList");
+// const url = "https://mandarin.api.weniv.co.kr";
+// let imageUrl;
 
-// 뒤로 가기
-backHistory.addEventListener("click", () => {
-  window.location = document.referrer;
-});
+// // 뒤로 가기
+// backHistory.addEventListener("click", () => {
+//   window.location = document.referrer;
+// });
 
-// 상품 이미지 미리보기 / api서버에 전송하고 변경된 이미지 이름 가져오기 
+// 상품 이미지 미리보기 / api서버에 전송하고 변경된 이미지 이름 가져오기
 
-async function onChangeProductImg(event) {
-  const imgReader = new FileReader();
-  const formData = new FormData();
+class AddProduct {
+  onChangeProductImg = async function (event) {
+    const imgReader = new FileReader();
+    const formData = new FormData();
 
-  if (event.target.files && event .target.files[0]) {
-    imgReader.onload = function () {
-      document.querySelector(".addProductUplodeImgScreen").src = imgReader.result;
-    };
-    imgReader.readAsDataURL(event.target.files[0]);
-  }
+    if (event.target.files && event.target.files[0]) {
+      imgReader.onload = function () {
+        document.querySelector(".addProductUplodeImgScreen").src =
+          imgReader.result;
+      };
+      imgReader.readAsDataURL(event.target.files[0]);
+    }
 
-  formData.append("image", event.target.files[0]);
-  try {
-    const response = await fetch(url + "/image/uploadfiles", {
-      method: "POST",
-      body: formData,
-    });
-    const data = await response.json();
-    imageUrl = await data[0].filename;
-    // console.log(imageUrl);
-  } catch (err) {
-    console.log(err);
-  }
-}
-// 상품 가격 숫자만, 자동 원화단위
+    formData.append("image", event.target.files[0]);
+    try {
+      const response = await fetch(url + "/image/uploadfiles", {
+        method: "POST",
+        body: formData,
+      });
+      const data = await response.json();
+      imageUrl = await data[0].filename;
+      // console.log(imageUrl);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  // 상품 가격 숫자만, 자동 원화단위
 
-function inputNumberFormat(addProductPrice) {
+  inputNumberFormat = (addProductPrice) => {
     addProductPrice.value = comma(uncomma(addProductPrice.value));
-}
+  };
 
-function comma(addProductPriceString) {
+  comma = (addProductPriceString) => {
     addProductPriceString = String(addProductPriceString);
     productInput();
-    return addProductPriceString.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,');
-}
+    return addProductPriceString.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, "$1,");
+  };
 
-function uncomma(addProductPriceString) {
+  uncomma = (addProductPriceString) => {
     addProductPriceString = String(addProductPriceString);
-    return addProductPriceString.replace(/[^\d]+/g, '');
-}
+    return addProductPriceString.replace(/[^\d]+/g, "");
+  };
 
-// 시간 선택
+  // productImg, productName, productPrice, productContents  모두 입력이 되면 헤더바에 저장 버튼 활성화
 
-timeSelelectBtn.addEventListener('click', () => {
-    timeSelelectBtn.classList.toggle('on');
-});
-
-timeSelectlist.addEventListener('click', (event) => {
-    if (event.target.nodeName === "BUTTON") {
-        timeSelelectBtn.textContent = event.target.textContent;
-        timeSelelectBtn.style.color = "black";
-        timeSelelectBtn.classList.remove('on');
+  // productSaveButton.disabled = true;
+  productInput = () => {
+    if (
+      productImgForm.value !== "" &&
+      productNameForm.value !== "" &&
+      productPriceForm.value !== "" &&
+      productContentForm.value !== ""
+    ) {
+      productSaveButton.style.opacity = "1";
+      productSaveButton.disabled = false;
+    } else {
+      productSaveButton.style.opacity = "0.3";
+      productSaveButton.disabled = true;
     }
-});
+  };
 
-// productImg, productName, productPrice, productContents  모두 입력이 되면 헤더바에 저장 버튼 활성화
+  // 상품 등록 모든 데이터 보내기
 
-productSaveButton.disabled = true;
-function productInput() {
-  
-  if (productImgForm.value !== "" && productNameForm.value !== "" && productPriceForm.value !=="" && productContentForm.value !=="") {
-    productSaveButton.style.opacity = "1";
-    productSaveButton.disabled = false;
-  } else {
-    productSaveButton.style.opacity = "0.3";
-    productSaveButton.disabled = true;
-  }
-}
-
-// 상품 등록 모든 데이터 보내기
-
-async function onProductSvaeBtn(e) {
-  const token = localStorage.getItem("Token");
+  onProductSvaeBtn = async function (e) {
+    const token = localStorage.getItem("Token");
     const productInfo = {
       product: {
         itemName: productNameForm.value,
-        price: parseInt(productPriceForm.value.replace(/,/g,"")),
+        price: parseInt(productPriceForm.value.replace(/,/g, "")),
         link: productContentForm.value,
-        itemImage: `${url}/${imageUrl}`
+        itemImage: `${url}/${imageUrl}`,
       },
     };
     const setting = {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${token}`,
+        Authorization: `Bearer ${token}`,
         "Content-type": "application/json",
       },
       body: JSON.stringify(productInfo),
@@ -111,9 +103,26 @@ async function onProductSvaeBtn(e) {
       const reqPosting = await fetch(`${url}/product`, setting);
       const resData = await reqPosting.json();
       console.log(resData);
-      location.href="../pages/profile.html";
+      location.href = "../pages/profile.html";
     } catch (error) {
       console.log(error);
     }
-  }
-  productSaveButton.addEventListener('click', onProductSvaeBtn);
+  };
+  // productSaveButton.addEventListener('click', onProductSvaeBtn);
+}
+
+export default AddProduct;
+
+// 시간 선택
+
+// timeSelelectBtn.addEventListener('click', () => {
+//   timeSelelectBtn.classList.toggle('on');
+// });
+
+// timeSelectlist.addEventListener('click', (event) => {
+//   if (event.target.nodeName === "BUTTON") {
+//       timeSelelectBtn.textContent = event.target.textContent;
+//       timeSelelectBtn.style.color = "black";
+//       timeSelelectBtn.classList.remove('on');
+//   }
+// });
--- script/pages/productPage.js
+++ script/pages/productPage.js
@@ -1,124 +1,186 @@
+import AddProduct from "../addProduct.js";
+
 class ProductPage {
   render() {
+    document.querySelector("body").classList.remove("profileBackground");
     // header
     const headerEl = document.createElement("header");
     const headerBarArticleEl = document.createElement("article");
     const backImgEl = document.createElement("img");
-    const titlePEl = document.createElement("p");
+    const saveBtnEl = document.createElement("button");
 
     headerBarArticleEl.classList.add("headerBar");
-    headerBarArticleEl.classList.add("followers");
+    headerBarArticleEl.classList.add("addProductHeaderBar");
     backImgEl.classList.add("headerBarBack");
     backImgEl.classList.add("buttonClick");
     backImgEl.setAttribute("src", "../asset/images/icons/icon__arrowLeft.svg");
     backImgEl.setAttribute("alt", "뒤로 가기");
-    titlePEl.classList.add("followTitle");
+    saveBtnEl.classList.add("headerBarSettingBtn");
+    saveBtnEl.classList.add("headerBarBtn");
+    saveBtnEl.classList.add("buttonClick");
+    saveBtnEl.id = "addProductSaveBtn";
+    saveBtnEl.setAttribute("type", "button");
+    saveBtnEl.innerText = "저장";
 
     headerBarArticleEl.appendChild(backImgEl);
-    headerBarArticleEl.appendChild(titlePEl);
+    headerBarArticleEl.appendChild(saveBtnEl);
     headerEl.appendChild(headerBarArticleEl);
 
-    return { headerEl };
+    //  main
+    const mainEl = document.createElement("main");
+    const addProductFormEl = document.createElement("form");
+    mainEl.classList.add("addProduct");
+    addProductFormEl.classList.add("addProductForm");
+
+    // // 이미지 등록
+    const addImgHeading3El = document.createElement("h3");
+    const addImgLabelEl = document.createElement("label");
+    const updateImgEl = document.createElement("img");
+    const addImgEl = document.createElement("img");
+    const addProductInput = document.createElement("input");
+
+    addImgHeading3El.classList.add("addProductFormLabel");
+    addImgHeading3El.innerText = "이미지 등록";
+    addImgLabelEl.classList.add("addProductFormUplodeImg");
+    addImgLabelEl.setAttribute("for", "addProductImg");
+    updateImgEl.classList.add("addProductUplodeImgScreen");
+    addImgEl.classList.add("addProductUplodeImg");
+    addImgEl.setAttribute(
+      "src",
+      "../asset/images/icons/icon__imagesUplode.svg"
+    );
+    addImgEl.setAttribute("alt", "이미지 등록");
+
+    addProductInput.id = "addProductImg";
+    addProductInput.setAttribute("type", "file");
+    addProductInput.setAttribute("accept", "image/*");
+    addProductInput.addEventListener("change", () => {
+      console.log("1");
+    });
+    addImgLabelEl.appendChild(updateImgEl);
+    addImgLabelEl.appendChild(addImgEl);
+    addProductFormEl.appendChild(addImgHeading3El);
+    addProductFormEl.appendChild(addImgLabelEl);
+    addProductFormEl.appendChild(addProductInput);
+
+    // // 상품명 등록
+    const addProductLabelEl = document.createElement("label");
+    const productNameInputEl = document.createElement("input");
+
+    addProductLabelEl.classList.add("addProductFormLabel");
+    addProductLabelEl.setAttribute("for", "addProductName");
+    addProductLabelEl.innerText = "상품명";
+    productNameInputEl.id = "addProductName";
+    productNameInputEl.setAttribute("type", "text");
+    productNameInputEl.required = true;
+    productNameInputEl.placeholder = "2~15자 이내여야 합니다.";
+    productNameInputEl.minLength = "2";
+    productNameInputEl.maxLength = "15";
+    productNameInputEl.addEventListener("keyup", () => {
+      console.log("1");
+    });
+
+    addProductFormEl.appendChild(addProductLabelEl);
+    addProductFormEl.appendChild(productNameInputEl);
+
+    // // 가격 등록
+    const addPriceLabelEl = document.createElement("label");
+    const addPriceInputEl = document.createElement("input");
+
+    addPriceLabelEl.classList.add("addProductFormLabel");
+    addPriceLabelEl.setAttribute("form", "addProductPrice");
+    addPriceLabelEl.innerText = "가격";
+    addPriceInputEl.id = "addProductPrice";
+    addPriceInputEl.setAttribute("type", "text");
+    addPriceInputEl.required = true;
+    addPriceInputEl.placeholder = "숫자만 입력 가능합니다.";
+    addPriceInputEl.maxLength = "12";
+    addPriceInputEl.addEventListener("keyup", () => {
+      console.log("1");
+    });
+
+    addProductFormEl.appendChild(addPriceLabelEl);
+    addProductFormEl.appendChild(addPriceInputEl);
+
+    // // 시간 선택
+    const timeSelectDivEl = document.createElement("div");
+    const timeSelectHeading3El = document.createElement("h3");
+    const timeSelectArticleEl = document.createElement("article");
+    const irHeading2El = document.createElement("h2");
+    const timeSelctBtnEl = document.createElement("button");
+    const timeUlEl = document.createElement("ul");
+    const timeLiEl10M = document.createElement("li");
+    const timeLiEl30M = document.createElement("li");
+    const timeLiEl1H = document.createElement("li");
+    const timeLiEl2H = document.createElement("li");
+    const timeLiEl3H = document.createElement("li");
+    const timeLiElShare = document.createElement("li");
+    const timeBtn10M = document.createElement("button");
+    const timeBtn30M = document.createElement("button");
+    const timeBtn1H = document.createElement("button");
+    const timeBtn2H = document.createElement("button");
+    const timeBtn3H = document.createElement("button");
+    const timeBtnShare = document.createElement("button");
+
+    timeSelectDivEl.classList.add("timeSelct");
+    timeSelectHeading3El.classList.add("addProductFormLabel");
+    timeSelectHeading3El.innerText = "시간 선택";
+    timeSelectArticleEl.classList.add("addProductTimeSelect");
+    irHeading2El.classList.add("ir");
+    irHeading2El.innerText = "시간을 선택해주세요.";
+    timeSelctBtnEl.classList.add("addProductSelectBtn");
+    timeSelctBtnEl.setAttribute("type", "button");
+    timeUlEl.classList.add("addProductTimeList");
+    timeBtn10M.innerText = "10분 이하";
+    timeBtn30M.innerText = "30분";
+    timeBtn1H.innerText = "1시간";
+    timeBtn2H.innerText = "2시간";
+    timeBtn3H.innerText = "3시간";
+    timeBtnShare.innerText = "나눔";
+
+    timeLiEl10M.appendChild(timeBtn10M);
+    timeLiEl30M.appendChild(timeBtn30M);
+    timeLiEl1H.appendChild(timeBtn1H);
+    timeLiEl2H.appendChild(timeBtn2H);
+    timeLiEl3H.appendChild(timeBtn3H);
+    timeLiElShare.appendChild(timeBtnShare);
+
+    timeUlEl.appendChild(timeLiEl10M);
+    timeUlEl.appendChild(timeLiEl30M);
+    timeUlEl.appendChild(timeLiEl1H);
+    timeUlEl.appendChild(timeLiEl2H);
+    timeUlEl.appendChild(timeLiEl3H);
+    timeUlEl.appendChild(timeLiElShare);
+
+    timeSelectArticleEl.appendChild(irHeading2El);
+    timeSelectArticleEl.appendChild(timeSelctBtnEl);
+    timeSelectArticleEl.appendChild(timeUlEl);
+    timeSelectDivEl.appendChild(timeSelectHeading3El);
+    timeSelectDivEl.appendChild(timeSelectArticleEl);
+    addProductFormEl.appendChild(timeSelectDivEl);
+
+    // // 내용 입력
+    const contentLabelEl = document.createElement("label");
+    const contentTextareaEl = document.createElement("textarea");
+
+    contentLabelEl.classList.add("addProductFormLabel");
+    contentLabelEl.innerText = "내용";
+    contentTextareaEl.cols = "10";
+    contentTextareaEl.rows = "10";
+    contentTextareaEl.id = "addProductContent";
+    contentTextareaEl.required = true;
+    contentTextareaEl.placeholder = "내용을 입력해 주세요.";
+    contentTextareaEl.addEventListener("click", () => {
+      console.log("1");
+    });
+
+    addProductFormEl.appendChild(contentLabelEl);
+    addProductFormEl.appendChild(contentTextareaEl);
+
+    mainEl.appendChild(addProductFormEl);
+
+    return { headerEl, mainEl };
   }
 }
 
 export default ProductPage;
-
-/* <header>
-      <article class="headerBar addProductHeaderBar">
-        <img
-          class="headerBarBack buttonClick"
-          src="../asset/images/icons/icon__arrowLeft.svg"
-          alt="뒤로가기"
-        />
-        <button type="button"
-        id="addProductSaveBtn"
-        class="headerBarSettingBtn headerBarBtn buttonClick"
-        >
-          저장
-        </button>
-      </article>
-    </header>
-
-    <main class="addProduct">
-        <!-- 이미지 등록 -->
-        <form class="addProductForm">
-          <h3 class="addProductFormLabel">이미지 등록</h3>
-          <label for="addProductImg" class="addProductFormUplodeImg">
-            <img class="addProductUplodeImgScreen">
-            <img class="addProductUplodeImg" src="../asset/images/icons/icon__imagesUplode.svg" alt="이미지 등록" />
-          </label>
-          <input 
-          id="addProductImg" 
-          type="file"
-          accept="image/*" 
-          onchange="onChangeProductImg(event)"
-          />
-
-        <!-- 상품명 등록 -->
-          <label class="addProductFormLabel" for="addProductName">상품명</label>
-          <input 
-          id="addProductName" 
-          type="text" 
-          required 
-          placeholder="2~15자 이내여야 합니다." 
-          minlength="2"
-          maxlength="15"
-          onkeyup="productInput()"
-          />
-        
-
-        <!-- 가격 등록 -->
-          <label class="addProductFormLabel" for="addProductPrice">가격</label>
-          <input
-            id="addProductPrice"
-            type="text"
-            required
-            placeholder="숫자만 입력 가능합니다."
-            maxlength="12"
-            onkeyup="inputNumberFormat(this)"
-            />
-            
-        <!-- 시간 선택 -->
-        <div class="timeSelct">
-          <h3 class="addProductFormLabel">시간 선택</h3>
-          <article class="addProductTimeSelect">
-            <h2 class="ir">시간을 선택해주세요</h2>
-            <button type="button" class="addProductSelectBtn">
-              시간을 선택해주세요.
-            </button>
-            <ul class="addProductTimeList">
-              <li>
-                <button type="button" class="textEllipsis">10분 이하</button>
-              </li>
-              <li><button type="button" class="textEllipsis">30분</button></li>
-              <li>
-                <button type="button" class="textEllipsis">1시간</button>
-              </li>
-              <li>
-                <button type="button" class="textEllipsis">2시간</button>
-              </li>
-              <li>
-                <button type="button" class="textEllipsis">3시간</button>
-              </li>
-              <li>
-                <button type="button" class="textEllipsis">나눔</button>
-              </li>
-            </ul>
-          </article>
-        </div>
-
-        <!-- 내용 입력 -->
-        
-          <label for="addProductContent" class="addProductFormLabel">내용</label>
-          <textarea
-            cols="10"
-            rows="10"
-            id="addProductContent"
-            required
-            placeholder="내용을 입력해 주세요."
-            onkeyup="productInput()"
-          ></textarea>
-        </form>
-      </main>
-    </div> */
