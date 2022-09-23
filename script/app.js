99e4decbf5c78bb11b5c639910b51c4f2f35e0d2
aydenote <dslgpgh@gmail.com>
2022-09-23 18:49:03 +0900

    게시물 업로드 페이지 SPA 마크업

--- script/app.js
+++ script/app.js
@@ -14,6 +14,7 @@ import {
   ChattingPage2,
   ChattingPage3,
   ChattingPage4,
+  PostPage,
 } from "./pages/index.js";
 import { Router } from "./utils/index.js";
 
@@ -41,6 +42,7 @@ export default class App {
       "/chatting2": ChattingPage2,
       "/chatting3": ChattingPage3,
       "/chatting4": ChattingPage4,
+      "/post": PostPage,
     });
     router.init(rootEl);
   }
---
+++ script/pages/postPage.js
@@ -0,0 +1,86 @@
+class PostPage {
+  render() {
+    document.querySelector("body").classList.remove("profileBackground");
+    // header
+    const headerEl = document.createElement("header");
+    const headerBarArticleEl = document.createElement("article");
+    const backImgEl = document.createElement("img");
+    const uploadBtnEl = document.createElement("button");
+
+    headerBarArticleEl.classList.add("headerBar");
+    headerBarArticleEl.classList.add("postingHeaderBar");
+    backImgEl.classList.add("headerBarBack");
+    backImgEl.classList.add("buttonClick");
+    backImgEl.setAttribute("src", "../asset/images/icons/icon__arrowLeft.svg");
+    backImgEl.setAttribute("alt", "뒤로 가기");
+    uploadBtnEl.classList.add("headerBarSettingBtn");
+    uploadBtnEl.classList.add("headerBarBtn");
+    uploadBtnEl.classList.add("buttonClick");
+    uploadBtnEl.id = "postingUploadBtn";
+    uploadBtnEl.setAttribute("type", "button");
+    uploadBtnEl.innerText = "업로드";
+
+    headerBarArticleEl.appendChild(backImgEl);
+    headerBarArticleEl.appendChild(uploadBtnEl);
+    headerEl.appendChild(headerBarArticleEl);
+
+    // main
+    const mainEl = document.createElement("main");
+    const postUploadFormEl = document.createElement("form");
+    const profileImgEl = document.createElement("img");
+    const postTextLabelEl = document.createElement("label");
+    const postTextareaEl = document.createElement("textarea");
+    const selectImgLabelEl = document.createElement("label");
+    const uploadImgEl = document.createElement("img");
+    const uploadInputEl = document.createElement("input");
+    const postImgDivEl = document.createElement("div");
+
+    mainEl.classList.add("postContainer");
+    postUploadFormEl.classList.add("postUploadForm");
+    profileImgEl.classList.add("userProfileImage");
+    profileImgEl.classList.add("postUploadCommentImg");
+    profileImgEl.setAttribute("alt", "프로필 이미지");
+    postTextLabelEl.classList.add("ir");
+    postTextLabelEl.setAttribute("for", "postUploadComent");
+    postTextLabelEl.innerText = "게시글 입력창입니다.";
+    postTextareaEl.setAttribute("type", "text");
+    postTextareaEl.id = "postUploadComent";
+    postTextareaEl.rows = "10";
+    postTextareaEl.maxLength = "140";
+    postTextareaEl.placeholder = "게시글 입력하기...";
+    postTextareaEl.classList.add("postUploadComentTxt");
+    postTextareaEl.addEventListener("keyup", () => {
+      //postInput()
+    });
+    selectImgLabelEl.classList.add("postUploadInputImg");
+    selectImgLabelEl.setAttribute("for", "chatSelectImage");
+    uploadImgEl.setAttribute(
+      "src",
+      "../asset/images/icons/icon__imagesUplode.svg"
+    );
+    uploadImgEl.setAttribute("alt", "");
+    uploadInputEl.setAttribute("type", "file");
+    uploadInputEl.id = "chatSelectImage";
+    uploadInputEl.accept = "image/*";
+    uploadInputEl.classList.add("postUploadInput");
+    uploadInputEl.classList.add("ir");
+    uploadInputEl.multiple = true;
+    uploadInputEl.addEventListener("change", () => {
+      // readInputFile(event)
+    });
+    postImgDivEl.classList.add("postUploadImageScreen");
+
+    selectImgLabelEl.appendChild(uploadImgEl);
+    postUploadFormEl.appendChild(profileImgEl);
+    postUploadFormEl.appendChild(postTextLabelEl);
+    postUploadFormEl.appendChild(postTextareaEl);
+    postUploadFormEl.appendChild(selectImgLabelEl);
+    postUploadFormEl.appendChild(uploadInputEl);
+    mainEl.appendChild(postUploadFormEl);
+    mainEl.appendChild(postImgDivEl);
+
+    return { headerEl, mainEl };
+  }
+}
+
+export default PostPage;
