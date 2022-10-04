1c2554195f10d96fcf5c32a6b7039bcf5f98703c
aydenote <dslgpgh@gmail.com>
2022-10-04 19:27:29 +0900

    불필요한 코드 삭제

--- script/search.js
+++ script/search.js
@@ -1,7 +1,5 @@
 import App from "./app.js";
 import Profile from "./profile.js";
-const profile = new Profile();
-const myAccountName = localStorage.getItem("accountname");
 
 const config = {
   rootEl: "#root",
--- script/signUp.js
+++ script/signUp.js
@@ -1,10 +1,10 @@
-const url = 'https://mandarin.api.weniv.co.kr';
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
-const errorEmail = document.querySelector('.errorEmail');
-const errorPassword = document.querySelector('.errorPassword');
-const registerForm = document.querySelector('.registerForm');
-const registerFormBtn = document.querySelector('.registerFormBtn');
+const url = "https://mandarin.api.weniv.co.kr";
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const errorEmail = document.querySelector(".errorEmail");
+const errorPassword = document.querySelector(".errorPassword");
+const registerForm = document.querySelector(".registerForm");
+const registerFormBtn = document.querySelector(".registerFormBtn");
 
 // 폼 입력
 class CheckForm {
@@ -15,12 +15,12 @@ class CheckForm {
 
   // 버튼 활성화
   isActiveBtn = () => {
-    if (email.value !== '' && password.value.length >= 6) {
-      registerFormBtn.style.opacity = '1';
+    if (email.value !== "" && password.value.length >= 6) {
+      registerFormBtn.style.opacity = "1";
       registerFormBtn.disabled = false;
     }
-    if (email.value === '' || password.value.length < 6) {
-      registerFormBtn.style.opacity = '0.3';
+    if (email.value === "" || password.value.length < 6) {
+      registerFormBtn.style.opacity = "0.3";
       registerFormBtn.disabled = true;
     }
   };
@@ -28,10 +28,10 @@ class CheckForm {
   // 입력 체크
   checkInput = () => {
     if (password.value.length < 6) {
-      errorPassword.classList.remove('ir');
+      errorPassword.classList.remove("ir");
     }
     if (password.value.length >= 6) {
-      errorPassword.classList.add('ir');
+      errorPassword.classList.add("ir");
       this.isActiveBtn();
     }
   };
@@ -45,28 +45,28 @@ class Validation {
 
   // 프로필 설정으로 이동
   locationRegisterProfile = (resMessage) => {
-    if (resMessage === '사용 가능한 이메일 입니다.') {
-      location.href = './registerProfile.html';
+    if (resMessage === "사용 가능한 이메일 입니다.") {
+      location.href = "./registerProfile.html";
     }
   };
 
   // 이메일, 비밀번호 로컬 스토리지에 저장
   saveData = (resMessage) => {
-    localStorage.setItem('email', email.value);
-    localStorage.setItem('password', password.value);
+    localStorage.setItem("email", email.value);
+    localStorage.setItem("password", password.value);
     this.locationRegisterProfile(resMessage);
   };
 
   // 사용 가능한 이메일 체크
   checkEmail = (resMessage) => {
-    if (resMessage !== '사용 가능한 이메일 입니다.') {
+    if (resMessage !== "사용 가능한 이메일 입니다.") {
       errorEmail.innerText = `*${resMessage}`;
-      errorEmail.classList.remove('ir');
+      errorEmail.classList.remove("ir");
       email.oninput = () => {
-        errorEmail.classList.add('ir');
+        errorEmail.classList.add("ir");
       };
     }
-    if (resMessage === '사용 가능한 이메일 입니다.') {
+    if (resMessage === "사용 가능한 이메일 입니다.") {
       this.saveData(resMessage);
     }
   };
@@ -94,6 +94,6 @@ const checkForm = new CheckForm(email, password);
 const validation = new Validation(email);
 
 // 폼 입력
-registerForm.addEventListener('input', checkForm.checkInput);
+registerForm.addEventListener("input", checkForm.checkInput);
 // 이메일 유효성 검사 버튼
-registerFormBtn.addEventListener('click', validation.isValidEmail);
+registerFormBtn.addEventListener("click", validation.isValidEmail);
