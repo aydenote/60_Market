c6625ef2679c1d05ded81d782d00036545af56bb
aydenote <dslgpgh@gmail.com>
2022-12-30 00:50:28 +0900

    테스트 케이스 작성, 불필요한 코드 삭제

--- script/addProduct.js
+++ script/addProduct.js
@@ -1,8 +1,3 @@
-import App from "./app.js";
-const config = {
-  rootEl: "#root",
-};
-
 // 상품 이미지 미리보기 / api서버에 전송하고 변경된 이미지 이름 가져오기
 class AddProduct {
   constructor(
@@ -13,117 +8,113 @@ class AddProduct {
     productSaveButton,
     timeSelctBtnEl
   ) {
-    this.productImgForm = productImgForm;
-    this.productNameForm = productNameForm;
-    this.productPriceForm = productPriceForm;
-    this.productContentForm = productContentForm;
-    this.productSaveButton = productSaveButton;
-    this.timeSelctBtnEl = timeSelctBtnEl;
-    this.imageUrl;
+    this.productImgForm = productImgForm
+    this.productNameForm = productNameForm
+    this.productPriceForm = productPriceForm
+    this.productContentForm = productContentForm
+    this.productSaveButton = productSaveButton
+    this.timeSelctBtnEl = timeSelctBtnEl
+    this.imageUrl
   }
 
   onChangeProductImg = async function (event) {
-    const imgReader = new FileReader();
-    const formData = new FormData();
-    const url = "https://mandarin.api.weniv.co.kr";
+    const imgReader = new FileReader()
+    const formData = new FormData()
+    const url = 'https://mandarin.api.weniv.co.kr'
 
     if (event.target.files && event.target.files[0]) {
       imgReader.onload = function () {
-        document.querySelector(".addProductUplodeImgScreen").src =
-          imgReader.result;
-      };
-      imgReader.readAsDataURL(event.target.files[0]);
+        document.querySelector('.addProductUplodeImgScreen').src = imgReader.result
+      }
+      imgReader.readAsDataURL(event.target.files[0])
     }
 
-    formData.append("image", event.target.files[0]);
+    formData.append('image', event.target.files[0])
     try {
-      const response = await fetch(url + "/image/uploadfiles", {
-        method: "POST",
+      const response = await fetch(url + '/image/uploadfiles', {
+        method: 'POST',
         body: formData,
-      });
-      const data = await response.json();
-      this.imageUrl = await data[0].filename;
-      await this.productInput();
+      })
+      const data = await response.json()
+      this.imageUrl = await data[0].filename
+      await this.productInput()
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
-  };
+  }
 
   // 상품 가격 숫자만, 자동 원화단위
   inputNumberFormat = () => {
-    this.productPriceForm.value = this.comma(
-      this.uncomma(this.productPriceForm.value)
-    );
-  };
+    this.productPriceForm.value = this.comma(this.uncomma(this.productPriceForm.value))
+    this.productInput()
+  }
 
-  comma = (addProductPriceString) => {
-    addProductPriceString = String(addProductPriceString);
-    this.productInput();
-    return addProductPriceString.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, "$1,");
-  };
+  comma = addProductPriceString => {
+    addProductPriceString = String(addProductPriceString)
+    return addProductPriceString.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,')
+  }
 
-  uncomma = (addProductPriceString) => {
-    addProductPriceString = String(addProductPriceString);
-    return addProductPriceString.replace(/[^\d]+/g, "");
-  };
+  uncomma = addProductPriceString => {
+    addProductPriceString = String(addProductPriceString)
+    return addProductPriceString.replace(/[^\d]+/g, '')
+  }
 
   // productImg, productName, productPrice, productContents  모두 입력이 되면 헤더바에 저장 버튼 활성화
   productInput = () => {
     if (
-      this.productImgForm.value !== "" &&
-      this.productNameForm.value !== "" &&
-      this.productPriceForm.value !== "" &&
-      this.productContentForm.value !== ""
+      this.productImgForm.value !== '' &&
+      this.productNameForm.value !== '' &&
+      this.productPriceForm.value !== '' &&
+      this.productContentForm.value !== ''
     ) {
-      this.productSaveButton.style.opacity = "1";
-      this.productSaveButton.disabled = false;
+      this.productSaveButton.style.opacity = '1'
+      this.productSaveButton.disabled = false
     } else {
-      this.productSaveButton.style.opacity = "0.3";
-      this.productSaveButton.disabled = true;
+      this.productSaveButton.style.opacity = '0.3'
+      this.productSaveButton.disabled = true
     }
-  };
+  }
 
   // 상품 등록 모든 데이터 보내기
   onProductSvaeBtn = async function () {
-    const token = localStorage.getItem("Token");
-    const url = "https://mandarin.api.weniv.co.kr";
+    const token = localStorage.getItem('Token')
+    const url = 'https://mandarin.api.weniv.co.kr'
     const productInfo = {
       product: {
         itemName: this.productNameForm.value,
-        price: parseInt(this.productPriceForm.value.replace(/,/g, "")),
+        price: parseInt(this.productPriceForm.value.replace(/,/g, '')),
         link: this.productContentForm.value,
         itemImage: `${url}/${this.imageUrl}`,
       },
-    };
+    }
     const setting = {
-      method: "POST",
+      method: 'POST',
       headers: {
         Authorization: `Bearer ${token}`,
-        "Content-type": "application/json",
+        'Content-type': 'application/json',
       },
       body: JSON.stringify(productInfo),
-    };
+    }
     try {
-      const reqPosting = await fetch(`${url}/product`, setting);
+      const reqPosting = await fetch(`${url}/product`, setting)
       if (reqPosting.status === 200) {
-        window.history.pushState({}, "", "/profile"); // 주소 업데이트
-        new App(config).setup();
+        window.history.back(1)
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   onTimeToggle = () => {
-    this.timeSelctBtnEl.classList.toggle("on");
-  };
-  onTimeSelct = (event) => {
-    if (event.target.nodeName === "BUTTON") {
-      this.timeSelctBtnEl.textContent = event.target.textContent;
-      this.timeSelctBtnEl.style.color = "black";
-      this.timeSelctBtnEl.classList.remove("on");
+    this.timeSelctBtnEl.classList.toggle('on')
+  }
+  onTimeSelct = event => {
+    if (event.target.nodeName === 'BUTTON') {
+      this.timeSelctBtnEl.textContent = event.target.textContent
+      this.timeSelctBtnEl.style.color = 'black'
+      this.timeSelctBtnEl.classList.remove('on')
     }
-  };
+  }
 }
 
-export default AddProduct;
+export default AddProduct
--- script/editProfile.js
+++ script/editProfile.js
@@ -1,18 +1,6 @@
-import App from "./app.js";
-const config = {
-  rootEl: "#root",
-};
-
 // 최초 프로필 정보 넣기
 class EditProfile {
-  constructor(
-    inputName,
-    inputId,
-    profileImg,
-    inputIntroduce,
-    alertMessage,
-    profileSaveButton
-  ) {
+  constructor(inputName, inputId, profileImg, inputIntroduce, alertMessage, profileSaveButton) {
     this.inputName = inputName;
     this.inputId = inputId;
     this.profileImg = profileImg;
@@ -22,26 +10,23 @@ class EditProfile {
     this.imageUrl;
   }
   getProfileInfo = async function () {
-    const url = "https://mandarin.api.weniv.co.kr";
-    const token = localStorage.getItem("Token");
-    const myAccountName = localStorage.getItem("accountname");
+    const url = 'https://mandarin.api.weniv.co.kr';
+    const token = localStorage.getItem('Token');
+    const myAccountName = localStorage.getItem('accountname');
 
     const setting = {
-      method: "GET",
+      method: 'GET',
       headers: {
         Authorization: `Bearer ${token}`,
-        "Content-type": "application/json",
+        'Content-type': 'application/json',
       },
     };
 
     try {
-      const resProfile = await fetch(
-        `${url}/profile/${myAccountName}`,
-        setting
-      );
+      const resProfile = await fetch(`${url}/profile/${myAccountName}`, setting);
       const resProfileJson = await resProfile.json();
       const userProfile = resProfileJson.profile;
-      this.profileImg.setAttribute("src", `${userProfile.image}`);
+      this.profileImg.setAttribute('src', `${userProfile.image}`);
 
       this.inputName.value = `${userProfile.username}`;
       this.inputId.value = `${userProfile.accountname}`;
@@ -57,34 +42,29 @@ class EditProfile {
     const inputIdLength = this.inputId.value.length;
     const inputIntroduceLength = this.inputIntroduce.value.length;
     const check = /^[a-zA-Z0-9_.]{1,10}$/;
-    this.alertMessage.innerText =
-      "*영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.";
+    this.alertMessage.innerText = '*영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.';
 
     // 만약 예외 문자가 없으면 ir 클래스 추가
     if (check.test(this.inputId.value)) {
-      this.alertMessage.classList.add("ir");
-      if (
-        inputNameLength >= 2 &&
-        inputIntroduceLength >= 2 &&
-        inputIdLength >= 2
-      ) {
-        this.profileSaveButton.style.opacity = "1";
+      this.alertMessage.classList.add('ir');
+      if (inputNameLength >= 2 && inputIntroduceLength >= 2 && inputIdLength >= 2) {
+        this.profileSaveButton.style.opacity = '1';
         this.profileSaveButton.disabled = false;
       } else {
-        this.profileSaveButton.style.opacity = "0.3";
+        this.profileSaveButton.style.opacity = '0.3';
         this.profileSaveButton.disabled = true;
       }
     }
 
     // 만약 예외 문자가 있으면 ir 클래스 제거
     if (!check.test(this.inputId.value)) {
-      this.alertMessage.classList.remove("ir");
-      this.profileSaveButton.style.opacity = "0.3";
+      this.alertMessage.classList.remove('ir');
+      this.profileSaveButton.style.opacity = '0.3';
       this.profileSaveButton.disabled = true;
     }
 
-    if (this.inputId.value === "") {
-      this.alertMessage.classList.add("ir");
+    if (this.inputId.value === '') {
+      this.alertMessage.classList.add('ir');
     }
   };
 
@@ -92,24 +72,24 @@ class EditProfile {
   imageChange = async function (e) {
     const imgReader = new FileReader();
     const formData = new FormData();
-    const url = "https://mandarin.api.weniv.co.kr";
+    const url = 'https://mandarin.api.weniv.co.kr';
 
     if (e.target.files[0].size > 10000000) {
-      alert("이미지 사이즈는 10MB 이내로 등록 가능합니다.");
+      alert('이미지 사이즈는 10MB 이내로 등록 가능합니다.');
       return;
     }
 
     if (e.target.files && e.target.files[0]) {
       imgReader.onload = function () {
-        document.querySelector(".updateUserImg").src = imgReader.result;
+        document.querySelector('.updateUserImg').src = imgReader.result;
       };
       imgReader.readAsDataURL(e.target.files[0]);
     }
 
-    formData.append("image", e.target.files[0]);
+    formData.append('image', e.target.files[0]);
     try {
-      const response = await fetch(url + "/image/uploadfiles", {
-        method: "POST",
+      const response = await fetch(url + '/image/uploadfiles', {
+        method: 'POST',
         body: formData,
       });
       const data = await response.json();
@@ -122,8 +102,8 @@ class EditProfile {
 
   // 저장 버튼 클릭 시 서버에 프로필 이미지, 이름, 소개, 계정 ID 전송
   clickSaveButton = async function () {
-    const token = localStorage.getItem("Token");
-    const url = "https://mandarin.api.weniv.co.kr";
+    const token = localStorage.getItem('Token');
+    const url = 'https://mandarin.api.weniv.co.kr';
 
     if (this.imageUrl === undefined) {
       this.imageUrl = this.profileImg.src;
@@ -137,10 +117,10 @@ class EditProfile {
       },
     };
     const setting = {
-      method: "PUT",
+      method: 'PUT',
       headers: {
         Authorization: `Bearer ${token}`,
-        "Content-type": "application/json",
+        'Content-type': 'application/json',
       },
       body: JSON.stringify(userProfileInfo),
     };
@@ -149,11 +129,10 @@ class EditProfile {
       const resEditProfileJson = await resEditProfile.json();
       // 응답 성공시
       if (resEditProfile.status === 200) {
-        localStorage.setItem("accountname", this.inputId.value);
-        window.history.pushState({}, "", "/profile"); // 주소 업데이트
-        new App(config).setup();
+        localStorage.setItem('accountname', this.inputId.value);
+        window.history.back(1);
       } else {
-        this.alertMessage.classList.remove("ir");
+        this.alertMessage.classList.remove('ir');
         this.alertMessage.innerText = `*${resEditProfileJson.message}`;
       }
     } catch (err) {
