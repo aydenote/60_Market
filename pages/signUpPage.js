a11a170484b3040b73ad88eafa5b33bd9d6e80b6
aydenote <dslgpgh@gmail.com>
2023-01-23 00:42:51 +0900

    클래스형에서 함수형으로 수정

--- pages/signUpPage.js
+++ pages/signUpPage.js
@@ -1,4 +1,4 @@
-import { CheckForm, Validation } from '../script/signUp.js';
+import { checkInput, isValidEmail } from '../script/signUp.js';
 
 class SignUpPage {
   render(content) {
@@ -76,15 +76,11 @@ class SignUpPage {
     loginButtonEl.innerText = '다음';
     formEl.appendChild(loginButtonEl);
 
-    const checkForm = new CheckForm(emailInputEl, passwordInputEl, passwordAlertPEl, loginButtonEl);
-    const validation = new Validation(emailInputEl, passwordInputEl, emailAlertPEl);
-
     // 폼 입력
-    formEl.addEventListener('input', checkForm.checkInput);
+    formEl.addEventListener('input', () => checkInput(passwordInputEl, passwordAlertPEl));
     // 이메일 유효성 검사 버튼
-    loginButtonEl.addEventListener('click', validation.isValidEmail);
+    loginButtonEl.addEventListener('click', () => isValidEmail());
 
-    // article
     const articleEl = document.createElement('article');
     const anchorEl = document.createElement('a');
 
--- script/signUp.js
+++ script/signUp.js
@@ -1,86 +1,70 @@
-const url = 'https://mandarin.api.weniv.co.kr';
-
-// 폼 입력
-export class CheckForm {
-  constructor(email, password, errorPassword, registerFormBtn) {
-    this.email = email;
-    this.password = password;
-    this.errorPassword = errorPassword;
-    this.registerFormBtn = registerFormBtn;
+// 버튼 활성화
+function isActiveBtn() {
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+  const registerFormBtn = document.querySelector('.registerFormBtn');
+  if (email.value !== '' && password.value.length >= 6) {
+    registerFormBtn.style.opacity = '1';
+    registerFormBtn.disabled = false;
   }
+  if (email.value === '' || password.value.length < 6) {
+    registerFormBtn.style.opacity = '0.3';
+    registerFormBtn.disabled = true;
+  }
+}
 
-  // 버튼 활성화
-  isActiveBtn = () => {
-    if (this.email.value !== '' && this.password.value.length >= 6) {
-      this.registerFormBtn.style.opacity = '1';
-      this.registerFormBtn.disabled = false;
-    }
-    if (this.email.value === '' || this.password.value.length < 6) {
-      this.registerFormBtn.style.opacity = '0.3';
-      this.registerFormBtn.disabled = true;
-    }
-  };
+// 입력 체크
+export function checkInput(password, errorPassword) {
+  if (password.value.length < 6) {
+    errorPassword.classList.remove('ir');
+  }
+  if (password.value.length >= 6) {
+    errorPassword.classList.add('ir');
+    isActiveBtn();
+  }
+}
 
-  // 입력 체크
-  checkInput = () => {
-    if (this.password.value.length < 6) {
-      this.errorPassword.classList.remove('ir');
-    }
-    if (this.password.value.length >= 6) {
-      this.errorPassword.classList.add('ir');
-      this.isActiveBtn();
-    }
-  };
+// 프로필 설정으로 이동
+function locationRegisterProfile(status) {
+  if (status === 200) {
+    window.location.hash = '#profileRegister';
+  }
+}
+
+// 이메일, 비밀번호 로컬 스토리지에 저장
+function saveData(res) {
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+  localStorage.clear();
+  localStorage.setItem('email', email.value);
+  localStorage.setItem('password', password.value);
+  locationRegisterProfile(res.status);
+}
+
+// 사용 가능한 이메일 체크
+function checkEmail(res) {
+  const email = document.getElementById('email');
+  const errorEmail = document.querySelector('.errorPassword');
+  if (res.data.message === '이미 가입된 이메일 주소 입니다.') {
+    errorEmail.innerText = `* 이미 가입된 이메일 주소 입니다.`;
+    errorEmail.classList.remove('ir');
+    email.oninput = () => {
+      errorEmail.classList.add('ir');
+    };
+  } else {
+    saveData(res);
+  }
 }
 
 // 이메일 유효성 검사
-export class Validation {
-  constructor(email, password, errorEmail) {
-    this.email = email;
-    this.errorEmail = errorEmail;
-    this.password = password;
-  }
+export async function isValidEmail() {
+  const emailInput = document.getElementById('email');
+  const url = 'https://mandarin.api.weniv.co.kr';
 
-  // 프로필 설정으로 이동
-  locationRegisterProfile = status => {
-    if (status === 200) {
-      window.location.hash = '#profileRegister';
-    }
-  };
-
-  // 이메일, 비밀번호 로컬 스토리지에 저장
-  saveData = status => {
-    // localStorage.clear();
-    localStorage.setItem('email', this.email.value);
-    localStorage.setItem('password', this.password.value);
-    this.locationRegisterProfile(status);
-  };
-
-  // 사용 가능한 이메일 체크
-  checkEmail = status => {
-    if (status !== 200) {
-      this.errorEmail.innerText = `*${resMessage}`;
-      this.errorEmail.classList.remove('ir');
-      this.email.oninput = () => {
-        this.errorEmail.classList.add('ir');
-      };
-    }
-    if (status === 200) {
-      this.saveData(status);
-    }
-  };
-
-  // 이메일 유효성 검사
-  isValidEmail = async () => {
-    try {
-      const res = await axios.post(`${url}/user/emailvalid`, {
-        user: {
-          email: this.email.value,
-        },
-      });
-      this.checkEmail(res.status);
-    } catch (error) {
-      this.checkEmail(error.response.data.message);
-    }
-  };
+  const res = await axios.post(`${url}/user/emailvalid`, {
+    user: {
+      email: emailInput.value,
+    },
+  });
+  checkEmail(res);
 }
