9e412a38ae14bc39aa0b16d30deb40aef07e0579
aydenote <dslgpgh@gmail.com>
2023-02-24 15:57:23 +0900

    타입스크립트로 프로필 수정 페이지 마크업

--- script/editProfile.ts
+++ script/editProfile.ts
@@ -1,6 +1,11 @@
 import { imageUpload, updateProfile } from '../utils/apiModule.js';
 
-export async function getProfileInfo(profileImg, inputName, inputId, inputIntroduce) {
+export async function getProfileInfo(
+  profileImg: HTMLImageElement,
+  inputName: HTMLInputElement,
+  inputId: HTMLInputElement,
+  inputIntroduce: HTMLInputElement
+) {
   const url = 'https://mandarin.api.weniv.co.kr';
   const token = localStorage.getItem('Token');
   const myAccountName = localStorage.getItem('accountname');
@@ -27,13 +32,13 @@ export async function getProfileInfo(profileImg, inputName, inputId, inputIntrod
   }
 }
 
-export function profileChangeInput(profileSaveButton) {
-  const inputNameLength = document.getElementById('name').value.length;
-  const inputId = document.getElementById('id');
+export function profileChangeInput(profileSaveButton: HTMLButtonElement) {
+  const inputNameLength = (document.getElementById('name') as HTMLInputElement).value.length;
+  const inputId = document.getElementById('id') as HTMLInputElement;
   const inputIdLength = inputId.value.length;
-  const inputIntroduceLength = document.getElementById('introduce').value.length;
+  const inputIntroduceLength = (document.getElementById('introduce') as HTMLInputElement).value.length;
   const check = /^[a-zA-Z0-9_.]{1,10}$/;
-  const alertMessage = document.querySelector('.alertMessage');
+  const alertMessage = document.querySelector('.alertMessage') as HTMLElement;
   alertMessage.innerText = '*영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.';
 
   // 만약 예외 문자가 없으면 ir 클래스 추가
@@ -61,32 +66,33 @@ export function profileChangeInput(profileSaveButton) {
 }
 
 // 프로필 이미지 교체 및 API서버에 이미지 전송하여 변경된 이미지 이름 가져오기
-export function imageChange(event) {
+export function imageChange(event: Event) {
   const imgReader = new FileReader();
   const formData = new FormData();
+  const file = (<HTMLInputElement>event.target).files;
 
-  if (event.target.files[0].size > 10000000) {
+  if (file![0].size > 10000000) {
     alert('이미지 사이즈는 10MB 이내로 등록 가능합니다.');
     return;
   }
 
-  if (event.target.files && event.target.files[0]) {
+  if (file && file[0]) {
     imgReader.onload = function () {
-      document.querySelector('.updateUserImg').src = imgReader.result;
+      (document.querySelector('.updateUserImg') as HTMLImageElement).src = `${imgReader.result}`;
     };
-    imgReader.readAsDataURL(event.target.files[0]);
+    imgReader.readAsDataURL(file[0]);
   }
 
-  formData.append('image', event.target.files[0]);
+  formData.append('image', file![0]);
   imageUpload(formData);
 }
 
 // 저장 버튼 클릭 시 서버에 프로필 이미지, 이름, 소개, 계정 ID 전송
-export function clickSaveButton(profileImg) {
-  const imageUrl = localStorage.getItem('imageUrl');
-
-  if (imageUrl === undefined) {
+export function clickSaveButton(profileImg: HTMLImageElement) {
+  let imageUrl = localStorage.getItem('imageUrl');
+  if (imageUrl === null) {
     imageUrl = profileImg.src;
+    localStorage.setItem('imageUrl', imageUrl);
   }
 
   updateProfile();
