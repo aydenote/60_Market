4dd2608a6b2ab5cef057540032ac40554e30115a
aydenote <dslgpgh@gmail.com>
2023-02-24 15:57:40 +0900

    타입스크립트로 하트 버튼 마크업

---
+++ script/heartBtn.ts
@@ -0,0 +1,19 @@
+import { likeHeart, likeUnHeart } from '../utils/apiModule.js';
+
+// 좋아요 버튼 클릭
+export async function clickHeart(event: MouseEvent) {
+  const likeBtn = event.target as HTMLElement;
+  const likeCount = likeBtn.children[1];
+  let postId = likeBtn.closest('section')!.id;
+  postId ? postId : (postId = new URLSearchParams(location.search).get('postid')!);
+
+  if (likeBtn.classList.contains('on')) {
+    likeBtn.classList.remove('on');
+    const data = await likeUnHeart(postId);
+    likeCount.innerHTML = await data.post.heartCount;
+  } else {
+    likeBtn.classList.add('on');
+    const data = await likeHeart(postId);
+    likeCount.innerHTML = data.post.heartCount;
+  }
+}
